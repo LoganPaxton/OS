171fc3ef3ed228b44a8e0db2bb89b92a5464317e
LoganPaxton <111173907+LoganPaxton@users.noreply.github.com>
2024-01-14 14:20:27 -0500

    Print Hello

--- boot.asm
+++ boot.asm
@@ -1,7 +1,15 @@
-; Infinte Loop
-loop:
-    jmp loop
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 ; Print L twice
+mov al, 'o'
+int 0x10
+
+jmp $
 
 times 510-($-$$) db 0
-
 dw 0xaa55
